f9972dbbe01edc503d2506cdda1a0e7b57ee1fdb
Eliza Drinker <elizaleedrinker@gmail.com>
2020-07-10 01:10:05 -0700

    Dockerized proxy

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+// const proxy = require('express-http-proxy');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/products', createProxyMiddleware({ target: 'http://ec2-54-176-165-31.us-west-1.compute.amazonaws.com/', changeOrigin: true }));
+app.use('/carousel', createProxyMiddleware({ target: 'http://ec2-54-177-202-87.us-west-1.compute.amazonaws.com', changeOrigin: true }));
+app.use('/questions', createProxyMiddleware({ target: 'http://ec2-54-219-98-209.us-west-1.compute.amazonaws.com', changeOrigin: true }));
+app.use('/reviews', createProxyMiddleware({ target: 'http://ec2-54-215-97-198.us-west-1.compute.amazonaws.com/', changeOrigin: true }));
+
+// app.use('/products', proxy('http://localhost:3001'));
+// app.use('/carousel', proxy('http://localhost:3002'));
+// app.use('/questions', proxy('http://localhost:3003'));
+// app.use('/reviews', proxy('http://localhost:3004'));
+
+app.get('/:index([0-9]|[0-9][0-9])', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,4 @@
-const express = require('express');
-const path = require('path');
-const proxy = require('express-http-proxy');
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.use('/product', proxy('http://localhost:3001'));
-app.use('/carousel', proxy('http://localhost:3002'));
-app.use('/questions', proxy('http://localhost:3003'));
-app.use('/reviews', proxy('http://localhost:3004'));
-
-app.get('/:index([0-9]|[0-9][0-9])', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+const app = require('./app');
 
 const PORT = 3000;
 
